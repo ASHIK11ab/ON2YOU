d4636228a33ec53c77c2feb57c4be8efd9991d3e
Ashik_Meeran_Mohideen <ashikrasheed7@gmail.com>
2020-06-25 20:39:30 +0530

    buy now & cart completed and some layouts completed

--- app.py
+++ app.py
@@ -1,112 +1,126 @@
 import pickle
 
-from flask import Flask, render_template, request ,redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 from myfunctions import*
 from myfunctions import seller, customer
 from jinja2.nativetypes import NativeEnvironment
 
-app= Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/seller')      #to redirect to login portal
+
+@app.route('/seller')  # to redirect to login portal
 def seller():
-    return render_template('sellerlogin.html', access_granted = True)
+    return render_template('sellerlogin.html', access_granted=True)
 
-@app.route('/sellernew') #to redirect to register portal
+
+@app.route('/sellernew')  # to redirect to register portal
 def sellernew():
-    return render_template('sellerregister.html', is_new_user = True, password_match = True)
+    return render_template('sellerregister.html', is_new_user=True, password_match=True)
 
-@app.route('/sellerlogin', methods = ["POST"])
+
+@app.route('/sellerlogin', methods=["POST"])
 def sellerlogin():
     username = request.form.get("username")
     password = request.form.get("password")
     access_granted = existingseller(username, password)
     if access_granted:
-        return redirect(url_for('recent_product', username = username, register = False))
+        return redirect(url_for('recent_product', username=username, register=False))
     else:
-        return render_template('sellerlogin.html', access_granted = access_granted)
+        return render_template('sellerlogin.html', access_granted=access_granted)
 
-@app.route('/sellerregister', methods = ["POST"])
+
+@app.route('/sellerregister', methods=["POST"])
 def sellerregister():
     username = request.form.get("username")
     password = request.form.get("password")
     confirm_password = request.form.get("confirm_password")
     if password != confirm_password:
         password_match = False
-        return render_template('sellerregister.html', is_new_user = True, password_match = password_match)
+        return render_template('sellerregister.html', is_new_user=True, password_match=password_match)
     else:
         password_match = True
         is_new_user = newseller(username, password)
         if is_new_user and password_match:
-            return redirect(url_for('recent_product', username = username, register = True))
+            return redirect(url_for('recent_product', username=username, register=True))
         else:
-            return render_template('sellerregister.html', is_new_user = is_new_user, password_match = password_match)
+            return render_template('sellerregister.html', is_new_user=is_new_user, password_match=password_match)
+
 
 @app.route('/add_item_render')
 def add_item_render():
     username = request.args.get('username')
-    return render_template('add_item.html', username = username, is_success = False)
+    return render_template('add_item.html', username=username, is_success=False)
+
 
 @app.route('/dashboard_seller')
 def recent_product():
     username = request.args.get('username')
     register = request.args.get('register')
     recent, count = recentproduct(username)
-    return render_template("recent_product.html", username = username, register = register, count = count, recent = recent)
+    return render_template("recent_product.html", username=username, register=register, count=count, recent=recent)
 
-@app.route('/dashboard_seller/add_item', methods = ["POST"])
+
+@app.route('/dashboard_seller/add_item', methods=["POST"])
 def add_item():
     products = {}
     products["item_name"] = request.form.get('item_name')
     products["category"] = request.form.get('category')
     products["cost"] = request.form.get('cost')
     username = request.args.get('username')
-    update_database(products,username)
-    return render_template('add_item.html', username = username, is_success = True)
+    update_database(products, username)
+    return render_template('add_item.html', username=username, is_success=True)
+
 
 @app.route('/dashboard_seller/all_products')
 def all_products():
     products = {}
     username = request.args.get('username')
-    products ,total = get_all_products(username)
-    return render_template('all_products.html', products = products, total = total, username= username)
+    products, total = get_all_products(username)
+    return render_template('all_products.html', products=products, total=total, username=username)
 
-@app.route('/customer')      #to redirect to login portal
+
+@app.route('/customer')  # to redirect to login portal
 def customer():
-    return render_template('customerlogin.html', access_granted = True)
+    return render_template('customerlogin.html', access_granted=True)
 
-@app.route('/customernew') #to redirect to register portal
+
+@app.route('/customernew')  # to redirect to register portal
 def customernew():
-    return render_template('customerregister.html', is_new_user = True, password_match = True)
+    return render_template('customerregister.html', is_new_user=True, password_match=True)
 
-@app.route('/customerlogin', methods = ["POST"])
+
+@app.route('/customerlogin', methods=["POST"])
 def customerlogin():
     username = request.form.get("username")
     password = request.form.get("password")
     access_granted = existingcustomer(username, password)
     if access_granted:
-        return redirect(url_for('recent_products_customer', message="none", is_message = False, username = username))
+        return redirect(url_for('recent_products_customer', message="none", is_message=False, username=username))
     else:
-        return render_template('customerlogin.html', access_granted = access_granted)
+        return render_template('customerlogin.html', access_granted=access_granted)
 
-@app.route('/customerregister', methods = ["POST"])
+
+@app.route('/customerregister', methods=["POST"])
 def customerregister():
     username = request.form.get("username")
     password = request.form.get("password")
     confirm_password = request.form.get("confirm_password")
     if password != confirm_password:
         password_match = False
-        return render_template('customerregister.html', is_new_user = True, password_match = password_match)
+        return render_template('customerregister.html', is_new_user=True, password_match=password_match)
     else:
         password_match = True
         is_new_user = newcustomer(username, password)
         if is_new_user and password_match:
-            return redirect(url_for('recent_products_customer',message="none", username = username, is_message = False))
+            return redirect(url_for('recent_products_customer', message="none", username=username, is_message=False))
         else:
-            return render_template('customerregister.html', is_new_user = is_new_user, password_match = password_match)
+            return render_template('customerregister.html', is_new_user=is_new_user, password_match=password_match)
+
 
 @app.route('/dashboard_customer')
 def recent_products_customer():
@@ -114,7 +128,8 @@ def recent_products_customer():
     message = request.args.get('message')
     username = request.args.get('username')
     results, total = recent_products_find()
-    return render_template('recent_products_customer.html', products = results, total = total, username = username, is_message = is_message , message = message)
+    return render_template('recent_products_customer.html', products=results, total=total, username=username, is_message=is_message, message=message)
+
 
 @app.route('/purchase')
 def purchase():
@@ -123,44 +138,91 @@ def purchase():
     t = e.from_string(product)
     product = t.render()
     username = request.args.get('username')
-    return render_template('purchase.html', product = product, username = username)
+    return render_template('purchase.html', product=product, username=username)
+
 
 @app.route('/Purchase/Purchase_tentative', methods=["POST"])
 def purchase_tentative():
     username = request.args.get('username')
     product = request.args.get('product')
     qty = request.form.get('quantity')
+    if qty == "":
+        qty = 1
     e = NativeEnvironment()
     t = e.from_string(product)
     product = t.render()
     product["total_cost"] = int(product["cost"])*int(qty)
     product["quantity"] = qty
-    return render_template('purchase_confirmation.html', product = product, username = username)
+    return render_template('purchase_confirmation.html', product=product, username=username)
+
 
 @app.route('/Purchase/Purchase_Confirmation')
 def purchase_confirmation():
     username = request.args.get('username')
     product = request.args.get('product')
+    product_cnt = request.args.get('product_cnt')
+    cart_cost = request.args.get('cart_cost')
     e = NativeEnvironment()
     t = e.from_string(product)
     product = t.render()
-    product["customer_name"] = request.args.get('username')
-    generatebill(product)
-    return redirect(url_for('recent_products_customer',is_message = True, message = "buy", username = username))
+    product["customer_name"] = username
+    product["no_of_items_in_cart"] = int(product_cnt)
+
+    if product["no_of_items_in_cart"] > 1:
+        product["cart_cost"] = cart_cost
+
+    product["mode"] = request.args.get('mode')
+    if product["mode"] == 'cart':
+        product["cart_cost"] = request.args.get('cart_cost')
+    
+    generatebill(product, username)
+    return redirect(url_for('recent_products_customer', is_message=True, message="buy", username=username))
+
 
 @app.route('/My_Bills')
 def mybills():
     username = request.args.get('username')
     bills, cnt = getmybill_ids(username)
-    return render_template('mybills.html', bills = bills, cnt = cnt, username = username)
+    return render_template('mybills.html', bills=bills, cnt=cnt, username=username)
+
 
 @app.route('/My_bills/Current_Bill')
 def display_current_bill():
     bill_id = request.args.get('bill_id')
     username = request.args.get('username')
-    bill, is_nested, total_no = get_this_bill(bill_id, username)
-    return render_template('current_bill.html', bill = bill, is_nested = is_nested, total_no = total_no)
+    bill = get_this_bill(bill_id, username)
+    return render_template('current_bill.html', bill=bill)
 
-@app.route('/add_to_cart')
+
+@app.route('/cart_render_portal')
+def cart_render_portal():
+    product = request.args.get('product')
+    e = NativeEnvironment()
+    t = e.from_string(product)
+    product = t.render()
+    username = request.args.get('username')
+    return render_template('cart.html', product=product, username=username)
+
+
+@app.route('/add_to_cart', methods=["POST"])
 def add_to_cart():
-    return "Added to cart"
+    username = request.args.get('username')
+    product = request.args.get('product')
+    qty = request.form.get('quantity')
+    if qty == "":
+        qty = 1
+    e = NativeEnvironment()
+    t = e.from_string(product)
+    product = t.render()
+    product["customer_name"] = username
+    product["total_cost"] = int(product["cost"])*int(qty)
+    product["quantity"] = qty
+    add_this_product_to_cart(product)
+    return redirect(url_for('recent_products_customer', is_message=True, message="cart", username=username))
+
+
+@app.route('/My_Cart')
+def my_cart():
+    username = request.args.get('username')
+    products, cnt, cart_cost = my_items_in_cart(username)
+    return render_template('mycart.html', products=products, cnt=cnt, username=username, cart_cost=cart_cost)
--- myfunctions.py
+++ myfunctions.py
@@ -1,4 +1,8 @@
-import pickle, datetime, random
+import pickle
+import datetime
+import random
+import os
+
 
 class seller:
     def getusername(self):
@@ -7,6 +11,7 @@ class seller:
     def getpassword(self):
         return self.password
 
+
 class customer:
     def getusername(self):
         return self.username
@@ -14,9 +19,10 @@ class customer:
     def getpassword(self):
         return self.password
 
+
 class products:
     def getusername(self):
-            return self.username
+        return self.username
 
     def getitem_name(self):
         return self.item_name
@@ -27,79 +33,86 @@ class products:
     def getcost(self):
         return self.cost
 
-def existingseller(name,password):
-        obj = seller()
-        is_existing = False
-        with open("database/seller.pkl", "rb") as file:
-            while True:
-                try:
-                    obj = pickle.load(file)
-                    if obj.getusername() == name:       #Checking for an existing user
-                        if obj.getpassword() == password:
-                            is_existing = True
-                            break
-                        else:
-                            break
-                except EOFError:
-                    break
-        return is_existing
+
+def existingseller(name, password):
+    obj = seller()
+    is_existing = False
+    with open("database/seller.pkl", "rb") as file:
+        while True:
+            try:
+                obj = pickle.load(file)
+                if obj.getusername() == name:  # Checking for an existing user
+                    if obj.getpassword() == password:
+                        is_existing = True
+                        break
+                    else:
+                        break
+            except EOFError:
+                break
+    return is_existing
+
 
 def newseller(name, password):
-        obj = seller()
-        is_new = True
-        with open("database/seller.pkl", "rb") as file:
-            while True:
-                try:
-                    obj = pickle.load(file)
-                    if obj.getusername() == name:   #Checking if there exists a user with the chosen username.
-                        is_new = False
-                        break
-                except EOFError:
+    obj = seller()
+    is_new = True
+    with open("database/seller.pkl", "rb") as file:
+        while True:
+            try:
+                obj = pickle.load(file)
+                # Checking if there exists a user with the chosen username.
+                if obj.getusername() == name:
+                    is_new = False
                     break
-        if is_new:  #if new user add this seller to database 'seller.pkl'
-            with open("database/seller.pkl", "a+b") as f:
-                object = seller()
-                object.username = name
-                object.password = password
-                pickle.dump(object, f)
-        return is_new
+            except EOFError:
+                break
+    if is_new:  # if new user add this seller to database 'seller.pkl'
+        with open("database/seller.pkl", "a+b") as f:
+            object = seller()
+            object.username = name
+            object.password = password
+            pickle.dump(object, f)
+    return is_new
 
-def existingcustomer(name,password):
-        obj = customer()
-        is_existing = False
-        with open("database/customer.pkl", "r+b") as file:
-            while True:
-                try:
-                    obj = pickle.load(file)
-                    if obj.getusername() == name:       #Checking for an existing user
-                        if obj.getpassword() == password:
-                            is_existing = True
-                            break
-                        else:
-                            break
-                except EOFError:
-                    break
-        return is_existing
+
+def existingcustomer(name, password):
+    obj = customer()
+    is_existing = False
+    with open("database/customer.pkl", "r+b") as file:
+        while True:
+            try:
+                obj = pickle.load(file)
+                if obj.getusername() == name:  # Checking for an existing user
+                    if obj.getpassword() == password:
+                        is_existing = True
+                        break
+                    else:
+                        break
+            except EOFError:
+                break
+    return is_existing
+
 
 def newcustomer(name, password):
-        obj =customer()
-        is_new = True
-        with open("database/customer.pkl", "rb") as file:
-            while True:
-                try:
-                    obj = pickle.load(file)
-                    if obj.getusername() == name:   #Checking if there exists a user with the chosen username.
-                        is_new = False
-                        break
-                except EOFError:
+    obj = customer()
+    is_new = True
+    with open("database/customer.pkl", "rb") as file:
+        while True:
+            try:
+                obj = pickle.load(file)
+                # Checking if there exists a user with the chosen username.
+                if obj.getusername() == name:
+                    is_new = False
                     break
-        if is_new:  #if new user add this seller to database 'seller.pkl'
-            with open("database/customer.pkl", "a+b") as f:
-                object = customer()
-                object.username = name
-                object.password = password
-                pickle.dump(object, f)
-        return is_new
+            except EOFError:
+                break
+    if is_new:  # if new user add this seller to database 'seller.pkl'
+        with open("database/customer.pkl", "a+b") as f:
+            object = customer()
+            object.username = name
+            object.password = password
+            pickle.dump(object, f)
+    return is_new
+
 
 def update_database(myproducts, username):
     obj = products()
@@ -109,6 +122,7 @@ def update_database(myproducts, username):
         obj.category = myproducts["category"]
         obj.cost = myproducts["cost"]
         pickle.dump(obj, file)
+
 
 def get_all_products(username):
     cnt = 0
@@ -120,14 +134,15 @@ def get_all_products(username):
                 obj = pickle.load(file)
                 if obj.getusername() == username:
                     subresults = {}
-                    cnt+= 1
+                    cnt += 1
                     subresults["item_name"] = obj.getitem_name()
                     subresults["category"] = obj.getcategory()
                     subresults["cost"] = obj.getcost()
                     results[cnt] = subresults
             except EOFError:
                 break
-    return (results,cnt)
+    return (results, cnt)
+
 
 def recentproduct(username):
     obj = products()
@@ -138,13 +153,14 @@ def recentproduct(username):
             try:
                 obj = pickle.load(file)
                 if obj.getusername() == username:
-                    cnt+= 1
+                    cnt += 1
                     recent["item_name"] = obj.getitem_name()
                     recent["category"] = obj.getcategory()
                     recent["cost"] = obj.getcost()
             except EOFError:
                 break
     return (recent, cnt)
+
 
 def recent_products_find():
     cnt = 0
@@ -155,7 +171,7 @@ def recent_products_find():
             try:
                 obj = pickle.load(file)
                 subresults = {}
-                cnt+= 1
+                cnt += 1
                 subresults["seller_name"] = obj.getusername()
                 subresults["item_name"] = obj.getitem_name()
                 subresults["category"] = obj.getcategory()
@@ -163,84 +179,108 @@ def recent_products_find():
                 results[cnt] = subresults
             except EOFError:
                 break
-    return (results,cnt)
+    return (results, cnt)
+
 
 def generateid():
-    id = random.randrange(1,100000)
+    id = random.randrange(1, 100000)
     return id
 
-def generatebill(product):
+
+def generatebill(product, username):
     now = datetime.datetime.now()
-    product["date"] = now.date
+    product["date"] = now.date()
     flag = 0
     cnt = 0
-    with open("database/id.pkl","rb") as file:
+    with open("database/id.pkl", "rb") as file:
         existing_ids = []
         while True:
             try:
                 existing_ids = pickle.load(file)
-                cnt+=1
+                cnt += 1
                 while True:
                     id = generateid()
-                    print("id is: ",id)
+                    print("id is: ", id)
                     if id not in existing_ids:
                         product["bill_id"] = id
                         existing_ids.append(id)
                         flag = 1
                         break
             except EOFError:
-                 break
+                break
     if cnt == 0:
         id = generateid()
         product["bill_id"] = id
         existing_ids.append(id)
         flag = 1
-        print("id is: ",id)
+        print("id is: ", id) 
 
     if flag == 1:
-        with open('database/id.pkl',"wb") as file:
-            pickle.dump(existing_ids,file)
+        with open('database/id.pkl', "wb") as file:
+            pickle.dump(existing_ids, file)
 
-        with open('database/bill.pkl',"ab") as file:
+        with open('database/bill.pkl', "ab") as file:
             pickle.dump(product, file)
 
+    if product["mode"] == 'cart':
+        with open('database/cart.pkl', "rb") as source:
+            with open('database/temporary.pkl', "wb") as destination:
+                while True:
+                    try: 
+                        data = pickle.load(source)
+                        if data["customer_name"] != username:
+                            pickle.dump(data, destination)
+                    except EOFError:
+                        break
+        os.chdir('database')
+        os.remove('cart.pkl')
+        os.rename('temporary.pkl','cart.pkl')
+        os.chdir('../')
+        
 def getmybill_ids(username):
-    with open("database/bill.pkl","rb") as file:
+    with open("database/bill.pkl", "rb") as file:
         id = []
         cnt = 0
         while True:
             try:
                 data = pickle.load(file)
-                try:
-                    if data[1]["customer_name"] == username:
-                        id.append(data["bill_id"])
-                        cnt+=1
-                except KeyError:
-                    if data["customer_name"] == username:
-                        id.append(data["bill_id"])
-                        cnt+=1
+                if data["customer_name"] == username:
+                    id.append(data["bill_id"])
+                    cnt += 1
             except EOFError:
                 break
     return id, cnt
 
+
 def get_this_bill(bill_id, username):
     with open("database/bill.pkl", "rb") as file:
-        is_nested = False
         while True:
             try:
                 data = pickle.load(file)
-                print(data)
                 if data["bill_id"] == int(bill_id):
                     item = data
-                    try:
-                        if data["1"]["customer_name"] == username:
-                            is_nested = True
-                            break
-                    except KeyError:
-                        break
+            except EOFError:
+                break    
+    return item
+
+
+def add_this_product_to_cart(product):
+    with open("database/cart.pkl", "ab") as file:
+        pickle.dump(product, file)
+
+
+def my_items_in_cart(username):
+    results = {}
+    cnt = 0
+    cost = 0
+    with open("database/cart.pkl", "rb") as file:
+        while True:
+            try:
+                data = pickle.load(file)
+                if data["customer_name"] == username:
+                    cnt += 1
+                    results[cnt] = data
+                    cost += data["total_cost"]
             except EOFError:
                 break
-    if is_nested:
-        return (item, is_nested, max(data.keys()))
-    else:
-        return (item, is_nested, 0)
+    return (results, cnt, cost)
